929192a7b355ea787b1403d8cbe25f03aac43554
Daniele Bracale <93348258+dbracale@users.noreply.github.com>
2022-10-23 11:18:11 -0400

    Update preprocessing.R

--- preprocessing.R
+++ preprocessing.R
@@ -1,15 +1,41 @@
-# these lines can be replaced by daniele's code
-set.seed(30001)
-n_ctrl_bananas <- n_tomato_bananas <- n_apple_bananas <- 8
-n_bananas <- n_ctrl_bananas + n_tomato_bananas + n_apple_bananas
-test_data <- data.frame(
-  "banana" = seq_len(n_bananas),
-  "trt" = rep(c("C", "A", "T"), each = n_bananas / 3),
-  "avg_rgb" = rnorm(n_bananas),
-  "pct_brown" = runif(n_bananas),
-  "pct_squished" = runif(n_bananas)
-)
+library(imager)
+library(dplyr)
 
-# these can be kept, but switch out test_data with the name of daniele's final dataframe
-data_to_write <- test_data
-write.csv(data_to_write, "aggregated_measurements.csv", row.names = FALSE)
+proportion_black = c()
+
+for (i in 1:36) {
+  #Take the pictures from the folder containing the banana's pics
+banana = load.image(paste("~/Dropbox (University of Michigan)/R/Stats 604/604 project 3/Day_1/",i,".jpg",sep = ""))
+#plot(banana)
+#class(banana)
+#dim(banana)
+
+  # Transform the bananapic in a data frame
+bdf_1 = as.data.frame(banana)
+
+  # divide by 3 because there are 3 channels, each of them are repeated (RGB)
+total_area = dim(bdf_1)[1]/3
+white_area = dim(filter(bdf_1, value == 1))[1]/3
+banana_area = total_area - white_area
+
+  # sqrt because it helps in dividing the black sports from the white.
+  # Transform the bananapic in a greyscale
+gbanana = sqrt(grayscale((banana)))
+#plot(gbanana)
+bdf_2 = as.data.frame(gbanana)
+  # Select the most black spots
+black_spots = which(bdf_2$value<0.5)
+  # set to white all the other spots (only if you what to see the next plot
+#bdf_2[-black_spots,3]= 1
+
+black_area = length(black_spots)
+
+#as.cimg(bdf_2, dims = dim(gbanana)) %>% plot
+#aggregate(bdf, list(channel = bdf$cc),FUN = mean)
+
+proportion_black = c(proportion_black, black_area/banana_area)
+}
+
+data = as.data.frame(proportion_black)
+colnames(data) = c("proportion_black")
+write.csv(data,"~/Dropbox (University of Michigan)/R/Stats 604/604 project 3/Day_1.csv", row.names = FALSE)
