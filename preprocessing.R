614dfa8a0fe75b2d97f7d4b18691acb730ed008e
Daniele Bracale <93348258+dbracale@users.noreply.github.com>
2022-10-26 15:03:01 -0400

    Update preprocessing.R

--- preprocessing.R
+++ preprocessing.R
@@ -1,17 +1,16 @@
 library(imager)
 library(dplyr)
 
-data <- data.frame("banana" = seq_len(36),
+data_black <- data.frame("banana" = seq_len(36),
+                   "trt" = rep(c("C", "A", "T"), each = 12))
+data_yellow <- data.frame("banana" = seq_len(36),
                    "trt" = rep(c("C", "A", "T"), each = 12))
 for (j in 1:5) {
   proportion_black = c()
+  proportion_yellow = c()
   for (i in 1:36) {
     #Take the pictures from the folder containing the banana's pics
-    if (j == 1) {
       banana = load.image(paste("Day_", j, "/",i,".jpg",sep = ""))
-    } else {
-      banana = load.image(paste("Day_", j, "/banana_",i,".jpeg",sep = ""))
-    }
     
     #plot(banana)
     #class(banana)
@@ -36,21 +35,23 @@ for (j in 1:5) {
     #bdf_2[-black_spots,3]= 1
     
     black_area = length(black_spots)
-    
+    yellow_area = banana_area - length(black_spots)
     #as.cimg(bdf_2, dims = dim(gbanana)) %>% plot
     #aggregate(bdf, list(channel = bdf$cc),FUN = mean)
     
     proportion_black = c(proportion_black, black_area/banana_area)
+    proportion_yellow = c(proportion_yellow, yellow_area/banana_area)
   }
-  data <- cbind(data, proportion_black)
+  data_black <- cbind(data_black, proportion_black)
+  data_yellow <- cbind(data_yellow, proportion_yellow)
 }
 
-# data = as.data.frame(proportion_black)
-# colnames(data) = c("proportion_black")
-colnames(data) <- c("banana", "trt", paste0("pct_brown_day_", seq(1, 5)))
-# write.csv(data,"Day_1.csv", row.names = FALSE)
+colnames(data_black) <- c("banana", "trt", paste0("pct_brown_day_", seq(1, 5)))
+colnames(data_yellow) <- c("banana", "trt", paste0("pct_brown_day_", seq(1, 5)))
 
 # read in squish results
-squish <- read.csv("processed_results/squish.csv", row.names = NULL)
-data <- cbind(data, pct_squished = squish$squish)
-write.csv(data, "processed_results/aggregated_measurements.csv", row.names = FALSE)
+# squish <- read.csv("processed_results/squish.csv", row.names = NULL)
+# data <- cbind(data, pct_squished = squish$squish)
+write.csv(data_black, "processed_results/aggregated_measurements_black.csv", row.names = FALSE)
+write.csv(data_yellow, "processed_results/aggregated_measurements_yellow.csv", row.names = FALSE)
+```
